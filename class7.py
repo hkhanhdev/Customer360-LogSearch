762dcac68c436a19ea5ad3d5d7f46e9c2e10a8e2
hoang <nguyenxuanhakhanh88060@gmail.com>
2025-05-16 22:08:03 +0700

    update ETL code

--- class7.py
+++ class7.py
@@ -2,7 +2,7 @@ import os,datetime as dt
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col,when,concat_ws, to_date, date_format,lower,monotonically_increasing_id,lit
 from pathlib import Path
-
+import tar_connections as tar_conn
 # Initialize SparkSession
 spark = SparkSession.builder.appName("ReadParquet").getOrCreate()
 
@@ -82,142 +82,133 @@ def writeCSV(df_final,where):
 		print(f"Getting error when saving result: {e}")
 		return False
 
-def processingT6(t6_dataset):
-	union_result = None 
-	for folder,files in t6_dataset.items():
-		for file_name in files:
-			df = readParquet(os.path.join(source_path,folder,file_name))
-			#Convert col with array<String> type to String type
-			result = df.withColumn("userPlansMap", concat_ws(",", col("userPlansMap")))
-			# Step 1: Filter out rows where category == 'quit'
-			df_filtered = result.filter(col("category") != "quit")
-			# Step 2: Drop the 'category' and 'action' columns
-			df_filtered = df_filtered.drop("category", "action")
-			# Step 3: Split 'datetime' into 'date' and 'time'
-			df_filtered = df_filtered.withColumn("date", to_date(col("datetime"))) 
-			df_filtered = df_filtered.withColumn("month", date_format(col("date"), "MM"))
-			# Step 4: Remove outlier
-			df_filtered = df_filtered.filter((col('month').isNotNull()) & (col('month') == 6)) 
-			# # Check for rows with NULL month or time -> resulted in col datetime is stored invalid dt format
-			# null_month = df_filtered.filter(col("month").isNull())
-			# null_month_count = null_month.count()
-			# if null_month_count > 0:
-			#     print(f"File {file_name} has {null_month_count} rows with NULL month")
-			#     null_month.show(1, truncate=False)
-			# outlier = df_filtered.filter(col("month") != 6)
-			# outlier_count = outlier.count()
-			# if outlier_count > 0:
-			#     print(f"File {file_name} has {outlier_count} rows with month value != 6")
-			#     outlier.show(1, truncate=False)
+def saveToDB(df_final):
+	print("-----------SAVING TO DATABASE-----------")
+	target_table = f"data_final"
+	connection_properties = {
+	    **tar_conn.T_CONN_PROPERTIES,
+	    "batchsize": "10000",
+	    "autoCommit": "true"
+	}
+	df_final.write.jdbc(
+	url=tar_conn.T_JDBC_URL,
+	table=f"{tar_conn.T_SCHEMA_NAME}.{target_table}",
+	mode="overwrite",
+	properties=connection_properties
+	)
+	print("---------------------------------------")
 
-			# Step 5: Normalize 'keyword' to lowercase
-			df_normalized = df_filtered.withColumn("keyword_normalized", lower(col("keyword")))
-			# Bước 6: Nhóm theo keyword, date, category và đếm số lượng tìm kiếm
-			search_counts = df_normalized.groupBy("keyword_normalized", "date",'month').count().withColumnRenamed('count','searches').withColumnRenamed('keyword_normalized','keyword')
-			search_counts = search_counts.orderBy(col('searches').desc())
-			# writeCSV(search_counts)
-			if union_result == None:
-				union_result = search_counts
-			else:
-				union_result = union_result.union(search_counts)
+def phase1(t6_dataset,t7_dataset):
+	datasets = {'t6':t6_dataset,'t7':t7_dataset}
+	union_t6result,union_t7result = None,None
+	for month,dataset in datasets.items():
+		for folder,files in dataset.items():
+			for file_name in files:
+				df = readParquet(os.path.join(source_path,folder,file_name))
+				#Convert col with array<String> type to String type
+				result = df.withColumn("userPlansMap", concat_ws(",", col("userPlansMap")))
 
-	df_final = union_result.groupBy("keyword",'month').sum().withColumnRenamed('sum(searches)','searches').orderBy(col('searches').desc())
-	return df_final                
+				# Step 1: Filter out rows where category == 'quit'
+				df_filtered = result.filter(col("category") != "quit")
+				# Step 2: Drop the 'category' and 'action' columns
+				df_filtered = df_filtered.drop("category", "action")
 
-def processingT7(t7_dataset):
-	union_result = None 
-	for folder,files in t7_dataset.items():
-		for file_name in files:
-			df = readParquet(os.path.join(source_path,folder,file_name))
-			#Convert col with array<String> type to String type
-			result = df.withColumn("userPlansMap", concat_ws(",", col("userPlansMap")))
-			# Step 1: Filter out rows where category == 'quit'
-			df_filtered = result.filter(col("category") != "quit")
-			# Step 2: Drop the 'category' and 'action' columns
-			df_filtered = df_filtered.drop("category", "action")
-			# Step 3: Split 'datetime' into 'date' and 'time'
-			df_filtered = df_filtered.withColumn("date", to_date(col("datetime"))) 
-			df_filtered = df_filtered.withColumn("month", date_format(col("date"), "MM"))
-			# Step 4: Remove outlier
-			df_filtered = df_filtered.filter((col('month').isNotNull()) & (col('month') == 7)) 
-			# # Check for rows with NULL month or time -> resulted in col datetime is stored invalid dt format
-			# null_month = df_filtered.filter(col("month").isNull())
-			# null_month_count = null_month.count()
-			# if null_month_count > 0:
-			#     print(f"File {file_name} has {null_month_count} rows with NULL month")
-			#     null_month.show(1, truncate=False)
-			# outlier = df_filtered.filter(col("month") != 6)
-			# outlier_count = outlier.count()
-			# if outlier_count > 0:
-			#     print(f"File {file_name} has {outlier_count} rows with month value != 6")
-			#     outlier.show(1, truncate=False)
+				# Step 3: Split 'datetime' into 'date' and 'time'
+				df_filtered = df_filtered.withColumn("date", to_date(col("datetime"))) 
+				df_filtered = df_filtered.withColumn("month", date_format(col("date"), "MM"))
+				# Step 4: Remove outlier
+				if month == 't6':
+					df_filtered = df_filtered.filter((col('month').isNotNull()) & (col('month') == 6)) 
+				elif month == 't7':
+					df_filtered = df_filtered.filter((col('month').isNotNull()) & (col('month') == 7))
 
-			# Step 5: Normalize 'keyword' to lowercase
-			df_normalized = df_filtered.withColumn("keyword_normalized", lower(col("keyword")))
-			# Bước 6: Nhóm theo keyword, date, category và đếm số lượng tìm kiếm
-			search_counts = df_normalized.groupBy("keyword_normalized", "date",'month').count().withColumnRenamed('count','searches').withColumnRenamed('keyword_normalized','keyword')
-			search_counts = search_counts.orderBy(col('searches').desc())
-			# writeCSV(search_counts)
-			if union_result == None:
-				union_result = search_counts
-			else:
-				union_result = union_result.union(search_counts)
+				# # Check for rows with NULL month or time -> resulted in col datetime is stored invalid dt format
+				# null_month = df_filtered.filter(col("month").isNull())
+				# null_month_count = null_month.count()
+				# if null_month_count > 0:
+				#     print(f"File {file_name} has {null_month_count} rows with NULL month")
+				#     null_month.show(1, truncate=False)
+				# outlier = df_filtered.filter(col("month") != 6)
+				# outlier_count = outlier.count()
+				# if outlier_count > 0:
+				#     print(f"File {file_name} has {outlier_count} rows with month value != 6")
+				#     outlier.show(1, truncate=False)
 
-	df_final = union_result.groupBy("keyword",'month').sum().withColumnRenamed('sum(searches)','searches').orderBy(col('searches').desc())
-	return df_final  
+				# Step 5: Normalize 'keyword' to lowercase
+				df_normalized = df_filtered.withColumn("keyword_normalized", lower(col("keyword")))
 
-def combiningResults():
-	t6_output_path = "D:\\Personal_Doc\\Class7\\result_t6\\result_t6.csv"
-	t7_output_path = "D:\\Personal_Doc\\Class7\\result_t7\\result_t7.csv"
-	# Bước 1: Đọc 2 file CSV
-	df1 = spark.read.csv(t6_output_path, header=True, inferSchema=True)
-	df2 = spark.read.csv(t7_output_path, header=True, inferSchema=True)
-	# Bước 2: Đổi tên cột để phân biệt giữa 2 DataFrame
+				# Bước 6: Nhóm theo keyword, date, category và đếm số lượng tìm kiếm
+				search_counts = df_normalized.groupBy("keyword_normalized", "date",'month').count()\
+					.withColumnRenamed('count','searches').withColumnRenamed('keyword_normalized','keyword')
+				search_counts = search_counts.orderBy(col('searches').desc())
+
+				if union_t6result == None and month == 't6':
+					union_t6result = search_counts
+				elif union_t6result != None and month == 't6':
+					union_t6result = union_t6result.union(search_counts)
+				elif union_t7result == None and month == 't7':
+					union_t7result = search_counts
+				elif union_t7result != None and month == 't7':
+					union_t7result = union_t7result.union(search_counts)
+
+	t6_df_final = union_t6result.groupBy("keyword",'month').sum()\
+		.withColumnRenamed('sum(searches)','searches')\
+		.orderBy(col('searches')\
+		.desc())
+	t7_df_final = union_t7result.groupBy("keyword",'month').sum()\
+		.withColumnRenamed('sum(searches)','searches')\
+		.orderBy(col('searches')\
+		.desc())
+
+	return t6_df_final.limit(100),t7_df_final.limit(100)              
+
+def phase2(t6,t7):
+	df1,df2 = t6,t7
+
 	df1 = df1.withColumnRenamed("keyword", "keyword_1") \
 	 .withColumnRenamed("month", "month_1") \
 	 .withColumnRenamed("searches", "searches_1")
-
 	df2 = df2.withColumnRenamed("keyword", "keyword_2") \
 	 .withColumnRenamed("month", "month_2") \
 	 .withColumnRenamed("searches", "searches_2")
 
-	# Bước 3: Thêm cột chỉ số (index) để ghép theo dòng
 	df1 = df1.withColumn("index", monotonically_increasing_id())
 	df2 = df2.withColumn("index", monotonically_increasing_id())
 
-	# Bước 4: Kết hợp 2 DataFrame theo cột index
-	combined_df = df1.join(df2, "index", "inner")
+	combined_df = df1.join(df2, on="index",how="inner")
 
-	# Bước 5: Chọn top 100 bản ghi và bỏ cột index
-	result_df = combined_df.drop("index").limit(100)
+	result_df = combined_df.drop('index')
 
-	# # Bước 6: Hiển thị kết quả
-	# result_df.show(100, truncate=False)
-	writeCSV(result_df,'combined_results_uncategorized')
+	# result_df.show(10)
+	# writeCSV(result_df,'combined_results_uncategorized')
+def phase3():
+	csv_path = "D:\\Class7\\result_categorized.csv"
+	df = readCSV(csv_path)
+	df = df.withColumn("trending_type",
+		when(col("keyword categorized_1") == col("keyword categorized_2"), "unchanged").otherwise("changed")
+	)
+	df = df.withColumn("changes",
+					when(col("trending_type") == "changed",
+					     concat_ws(" | ", col("keyword categorized_1"), col("keyword categorized_2"))
+					).otherwise(lit("unchanged"))
+	)
+
+	df = df.select("keyword_1",'keyword categorized_1','keyword_2','keyword categorized_2','trending_type','changes')
+	# writeCSV(df,'final_result')
+	saveToDB(df)
 
 def main():
-	# #Calculating
-	# t6,t7 = getListDir()
-	# t6 = processingT6(t6)
-	# t7 = processingT7(t7)
-	# writeCSV(t6,'result_t6')
-	# writeCSV(t7,'result_t7')
-	csv_path = "D:\\Personal_Doc\\Class7\\result_categorized.csv"
-	df = readCSV(csv_path)
-	df = df.withColumn(
-	"trending_type",
-	when(col("keyword categorized_1") == col("keyword categorized_2"), "unchanged").otherwise("changed")
-	)
+	start_time = dt.datetime.now()
 
-	df = df.withColumn(
-	"changes",
-	when(col("trending_type") == "changed",
-	     concat_ws(" | ", col("keyword categorized_1"), col("keyword categorized_2"))
-	).otherwise(lit("unchanged"))
-	)
-	df = df.select("keyword_1",'keyword categorized_1','keyword_2','keyword categorized_2','trending_type','changes')
-	writeCSV(df,'final_result')
-	# df.show(10)
+	t6,t7 = getListDir()
+	t6_p1,t7_p1 = phase1(t6,t7)
+	phase2(t6_p1,t7_p1)
+	phase3()
+
+	end_time = dt.datetime.now()
+	time_processing = (end_time - start_time).total_seconds()
+	print(f"It took {time_processing} seconds to process all the data")
+
 
 if __name__ == '__main__':
 	try:
